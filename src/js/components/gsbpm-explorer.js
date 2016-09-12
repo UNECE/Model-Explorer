7f49f022f7a0f018a946f66deda85bf50b1b8d87
romaintailhurat <romain.tailhurat@gmail.com>
2016-09-12 17:10:16 +0200

    Cleaning the GSBPMExplorer from fake data

--- src/js/components/gsbpm-explorer.js
+++ src/js/components/gsbpm-explorer.js
@@ -4,27 +4,6 @@ import { LOADING, LOADED, FAILED } from 'sparql-connect'
 import { groupByWithOrder } from '../utils/group-by'
 import GSBPMSubprocess from './gsbpm-subprocess'
 
-//FIXME temporary data structure use for mocking
-const _phases = {
-  'http:// phase1': {
-    props: { phaseLabel: 'This is the phase 1 of the GSBPM' },
-    entries: [
-      {
-        subprocess: 'http://subprocess11',
-        subprocessLabel: 'This is the subprocess 11'}
-    ]
-  },
-  'http://phase2' :{
-    props: { phaseLabel: 'This is the phase 2 of the GSBPM' },
-    entries: [
-      {
-        subprocess: 'http://subprocess21',
-        subprocessLabel: 'This is the subprocess 21'
-      }
-    ]
-  }
-}
-
 function GSBPMExplorer({ loaded, phases }) {
   if(loaded !== LOADED) {
     return <p>LOADING...</p>
