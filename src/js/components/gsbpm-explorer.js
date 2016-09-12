f29c2b7ecf079f791f3ae66e5986656ea546b56e
romaintailhurat <romain.tailhurat@gmail.com>
2016-09-12 15:35:05 +0200

    Using fake data for mocking GSBPM

--- src/js/components/gsbpm-explorer.js
+++ src/js/components/gsbpm-explorer.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
+//FIXME temporary data structure use for mocking
+const _phases = {
+  'http:// phase1': {
+    props: { phaseLabel: 'This is the phase 1 of the GSBPM' },
+    entries: [
+      {
+        subprocess: 'http://subprocess11',
+        subprocessLabel: 'This is the subprocess 11'}
+    ]
+  },
+  'http://phase2' :{
+    props: { phaseLabel: 'This is the phase 1 of the GSBPM' },
+    entries: [
+      {
+        subprocess: '',
+        subprocessLabel: 'This is the subprocess 11'
+      }
+    ]
+  }
+}
+
 function GSBPMExplorer({ loaded, phases }) {
   return (
     <ul>
-      { [1, 2, 3].map(() =>
-        <li>phases</li>
+      { Object.keys(_phases).map((i) =>
+        <li key={i}>{i} - {_phases[i].props.phaseLabel}</li>
       )}
     </ul>
   );
