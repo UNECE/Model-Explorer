6e44ae345a7e27c18b93682333b738d74ffc9cef
romaintailhurat <romain.tailhurat@gmail.com>
2016-09-12 16:27:01 +0200

    Using GSBPMSubprocess component and connecting to GSBPM query

--- src/js/components/gsbpm-explorer.js
+++ src/js/components/gsbpm-explorer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { sparqlConnect } from '../sparql/configure-sparql'
+import { LOADING, LOADED, FAILED } from 'sparql-connect'
+import { groupBy } from '../utils/group-by'
+import GSBPMSubprocess from './gsbpm-subprocess'
 
 //FIXME temporary data structure use for mocking
 const _phases = {
@@ -12,26 +15,40 @@ const _phases = {
     ]
   },
   'http://phase2' :{
-    props: { phaseLabel: 'This is the phase 1 of the GSBPM' },
+    props: { phaseLabel: 'This is the phase 2 of the GSBPM' },
     entries: [
       {
-        subprocess: '',
-        subprocessLabel: 'This is the subprocess 11'
+        subprocess: 'http://subprocess21',
+        subprocessLabel: 'This is the subprocess 21'
       }
     ]
   }
 }
 
 function GSBPMExplorer({ loaded, phases }) {
+  if(loaded !== LOADED) {
+    return <p>LOADING...</p>
+  }
+  const refinedPhases = groupBy(phases, 'phase', 'phaseLabel')
+  console.dir(refinedPhases)
   return (
     <ul>
-      { Object.keys(_phases).map((i) =>
-        <li key={i}>{i} - {_phases[i].props.phaseLabel}</li>
+      { Object.keys(refinedPhases).map((i) =>
+        <li key={i}>
+          {i} - {refinedPhases[i].props.phaseLabel}
+          <ul>
+          { refinedPhases[i].entries.map((sp) =>
+            <li key={sp.subprocess}>
+              <GSBPMSubprocess
+                id={sp.subprocess}
+                label={sp.subprocessLabel} />
+            </li>
+          )}
+          </ul>
+        </li>
       )}
     </ul>
   );
 }
-
-
 
 export default sparqlConnect.GSBPMDescription(GSBPMExplorer);
---
+++ src/js/components/gsbpm-subprocess.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GSBPMSubprocess(props) {
+  return(
+    <p>{props.id} - {props.label}</p>
+  )
+}
+
+export default GSBPMSubprocess;
