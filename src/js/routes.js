bb08c0e852173e189829519478c9fefa9e8bbe4a
romaintailhurat <romain.tailhurat@gmail.com>
2016-09-13 15:49:20 +0200

    Cleaning the routes definitions

--- src/js/routes.js
+++ src/js/routes.js
@@ -10,16 +10,6 @@ const prefix = 'http://stamina-project.org/codes'
 const rPrefix = new RegExp(prefix + '\/(.*)')
 
 const routes = {
-  // classificationDetails: {
-  //   pattern: '/classification/:classificationId/details/:conceptSchemeId',
-  //   paramsToProps: (state, { classificationId, conceptSchemeId })  => ({
-  //     classification: `${prefix}/${classificationId}/${conceptSchemeId}`
-  //   }),
-  //   uriToLink: uri => {
-  //     const [classifcationId, conceptSchemeId] = uri.match(rPrefix)[1].split('/')
-  //     return `/classification/${classifcationId}/details/${conceptSchemeId}`
-  //   }
-  // }
   serviceDetails: {
     pattern: 'service/:serviceId',
     paramsToProps: (state, { serviceId }) => ({
