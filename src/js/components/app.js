9025f0509861a54acec8122ae709c80bf4ebd051
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-09-13 16:37:05 +0200

    Add menu bar

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router'
 
+//TODO config routes for GSIM and GSBPM explorers in the dedictated file
 export default function App({ children }) {
   return (
     <div className="container-fluid">
+      <header>
+        Explore with:
+        <ul>
+          <li>
+            <Link to="/gsbpm">GSBPM</Link>
+          </li>
+          <li>
+            <Link to="/gsim">GSIM</Link>
+          </li>
+        </ul>
+
+      </header>
       <h1>Model Explorer</h1>
       {/* Children is a special prop name with react. Here, it allows
           `react-router` to populate our main component with the components
