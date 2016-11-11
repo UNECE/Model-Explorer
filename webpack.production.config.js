f51eaf0f3dcfa5b9486a3114f09fc695733d6940
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-11-11 09:46:32 +0100

    Fix pathnames issue in webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -29,9 +29,9 @@ module.exports = {
     extensions: ['', '.js']
   },
   output: {
-    path: __dirname + '/dist/js',
+    path: __dirname + '/dist',
     publicPath: 'js', // it depends on what we set as content-base option with
                       // the CLI
-    filename: 'model-explorer.js'
+    filename: './js/model-explorer.js'
   }
 }
