2a774a58d510bc23eb7216543f0b4f83a5b8b481
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-11-11 17:49:16 +0100

    Remove useless check on `loaded`

--- src/js/components/services-by-gsbpm-phase.js
+++ src/js/components/services-by-gsbpm-phase.js
@@ -4,7 +4,6 @@ import { LOADED } from 'sparql-connect'
 import ServiceList from './service-list'
 
 function ServicesByGSBPMPhase({ loaded, services }) {
-  if(loaded !== LOADED) return <span>loading......</span>
   return <ServiceList
     services={services}
     msg="No service implements this GSBPM phase"
