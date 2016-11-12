e77300885f7a79416c36f76205adb7f75c0d13f2
Franck Cotton <franck.cotton@insee.fr>
2016-11-11 20:11:03 -0500

    Added country code in the NSI list

--- src/js/components/nsi-list.js
+++ src/js/components/nsi-list.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
 export default function NSIList({ nsis }) {
-  
-  if (nsis.length === 0) 
+
+  if (nsis.length === 0)
     return (
       <div className="alert alert-warning" role="alert">
         Sorry, the NSI list is empty.
       </div>
     )
-    
+
   return(
     <ul className="list-group">
       { nsis.map(({ nsi, label }) =>
           <li className="list-group-item">
-            { label }
+            {nsi.slice(-2)}-{ label }
           </li>)
        }
     </ul>
