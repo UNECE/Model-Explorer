630c9da4642b799b8c8391ae99124866e5671625
Franck Cotton <franck.cotton@insee.fr>
2016-11-17 10:04:47 -0500

    Work on the NSI page

--- src/js/components/nsi-details.js
+++ src/js/components/nsi-details.js
@@ -6,11 +6,17 @@ import { LOADED } from 'sparql-connect'
 function NSIDetails({ nsi, loaded, name, shortName, address, geo }) {
   if (loaded !== LOADED) return <span>Loading...</span>
   var countryCode = nsi.slice(-2);
-  var flagClassName = "flag-icon flag-icon-" + countryCode.toLowerCase() +" large-flag";
+  var flagClassName = "flag-icon flag-icon-" + countryCode.toLowerCase() + " large-flag";
+  // Computes the OpenStreetMap URL corresponding to the coordinates (NB: 18 is the zoom level)
+  var osmURL = (geo) ? 'http://www.openstreetmap.org/#map=18/' + geo.substring(4).replace(',', '/') : '';
+  var osmLink = (address && geo) ? '<a href="' + osmURL + '"><img src="../src/img/osm.svg"/></a>' : '';
+
   return (
     <div>
-      <h1>{name + ((shortName) ? ' (' + shortName + ')' : '')}</h1>
       <span className={flagClassName}></span>
+      <h1>{name + ((shortName) ? ' (' + shortName + ')' : '')}</h1>
+      <h2>{address}</h2>
+      <p>{osmLink}</p>
     </div>
   )
 }
