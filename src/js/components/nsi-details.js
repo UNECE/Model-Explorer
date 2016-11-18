0e0eeda107567fe62ab7b353baada1761d2995fa
Franck Cotton <franck.cotton@insee.fr>
2016-11-18 08:26:32 -0500

    Added OSM link in the NSI details page

--- src/js/components/nsi-details.js
+++ src/js/components/nsi-details.js
@@ -9,14 +9,18 @@ function NSIDetails({ nsi, loaded, name, shortName, address, geo }) {
   var flagClassName = "flag-icon flag-icon-" + countryCode.toLowerCase() + " large-flag";
   // Computes the OpenStreetMap URL corresponding to the coordinates (NB: 18 is the zoom level)
   var osmURL = (geo) ? 'http://www.openstreetmap.org/#map=18/' + geo.substring(4).replace(',', '/') : '';
-  var osmLink = (address && geo) ? '<a href="' + osmURL + '"><img src="../src/img/osm.svg"/></a>' : '';
 
   return (
     <div>
       <span className={flagClassName}></span>
       <h1>{name + ((shortName) ? ' (' + shortName + ')' : '')}</h1>
-      <h2>{address}</h2>
-      <p>{osmLink}</p>
+
+      {address &&
+        <div>
+        {geo && (<a href={osmURL}><img src="/img/osm.png"/></a>)}
+        <h2>{address}</h2>
+        </div>
+       }
     </div>
   )
 }
