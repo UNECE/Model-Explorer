5bbf40392f796045a5cc5af008740b81371ca548
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-12-17 08:32:56 +0100

    Use `wrapRoute` only at the top of the routes hierarchy

--- src/js/components/nsi/routes.js
+++ src/js/components/nsi/routes.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { IndexRoute, Route } from 'react-router'
 import NSIExplorer from './explorer'
 import NSIDetails from './details'
-import { wrapRoute, transform } from '../../utils/router-mapping'
+import { transform } from '../../utils/router-mapping'
 
 export const linkNSI = transform(
   'http://id.unece.org/nsi/:nsi',
   '/nsis/:nsi'
 )
 
-export default wrapRoute(
+export default (
   <Route path="/nsis">
     <IndexRoute component={NSIExplorer} />
     <Route
