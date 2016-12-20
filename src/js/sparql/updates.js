31f1259798155f7086dbd61675e42572dcb73cdb
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-12-20 23:16:34 +0100

    Use `registerSetFetchQuery` for insert/udpate/remove operations

--- src/js/sparql/updates.js
+++ src/js/sparql/updates.js
@@ -1,19 +1,19 @@
 import { insertUpdateService } from '../sparql/insert-update-service-query'
 import { removeServiceQuery } from '../sparql/remove-service-query'
+import { registerSetFetchQuery } from '../utils/authentication'
 
 //At initialization, `fetchQuery` is not available since we need the
 //authentication token first (which will be set within the `authentication`
-//module). We create a placeholder for the `fetchQuery` function and we export
-//`setFetchQuery` to change the value of `fetchQuery` (it will be used by the
+//module). We create a placeholder for the `fetchQuery` function and we register
+//a function to set `fetchQuery` when available (it will be called by the
 //`authentication` module once the user is authenticated).
 let fetchQuery = null
-
-export const setFetchQueryUpdate = fn => fetchQuery = fn
+registerSetFetchQuery(fn => fetchQuery = fn)
 
 export function createService(descr) {
   const { query, serviceURI } = insertUpdateService(descr)
   return fetchQuery(query)
-          .then(() => serviceURI)
+    .then(() => serviceURI)
 }
  
 export function updateService(descr) {
