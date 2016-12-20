2830e28ff8e459d135c821a9c2cf4566cb660771
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-12-20 21:28:47 +0100

    Update documentation

--- src/js/sparql/remove-service-query.js
+++ src/js/sparql/remove-service-query.js
@@ -1,4 +1,6 @@
-/* Create a new service. This query is not intended to be used with 
-   sparqlConnect.
-*/
+/**
+  Returns the query to remove a service.
+  
+  Is is not supposed to be used with `sparql-connect`
+**/
 export const removeServiceQuery = serviceGraph => `DROP GRAPH <${serviceGraph}>`
