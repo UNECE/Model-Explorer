f392a649ba87e931467c0ece3277c61867cd9d90
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-12-20 16:19:00 +0100

    Fix typo

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ import Root from './components/root'
 //We can import css from javascript with webpack `style-loader`
 import '../css/style.css'
 import '../css/flag-icon-css/flag-icon.min.css'
-//Our app will be embeded in `index.html`; webpack file loader allow this
+//Our app will be embeded in `index.html`; webpack file loader allows this
 //kind of import to mark this 'dependence': `index.html` will be automatically
 //copy in the `dist` directory during the build process.
 import 'file?name=[name].[ext]!../index.html'
